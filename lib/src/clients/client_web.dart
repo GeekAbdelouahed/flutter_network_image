fd5cc9a470c0d4b9f8856101f57909bb574fce3c
Abdelouahed Medjoudja <abdelouahed@brandbassador.com>
2023-01-29 13:06:08 +0100

    feat: initial commit

--- lib/src/clients/client_web.dart
+++ lib/src/clients/client_web.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
+import 'dart:developer';
 import 'dart:typed_data';
 
-import '_client_web.dart'
-    if (dart.library.io) 'dart:io'
-    if (dart.library.html) 'dart:html' as html;
+import '_html.dart' if (dart.library.io) 'dart:io' as html;
 import 'base_client.dart';
 
 class HttpClient implements BaseHttpClient {
@@ -25,12 +24,13 @@ class HttpClient implements BaseHttpClient {
     request.onLoad.listen((event) {
       final int? status = request.status;
       final bool accepted = status! >= 200 && status < 300;
-      final bool fileUri = status == 0; // file:// URIs have status of 0.
+      final bool fileUri = status == 0;
       final bool notModified = status == 304;
       final bool unknownRedirect = status > 307 && status < 400;
       final bool success =
           accepted || fileUri || notModified || unknownRedirect;
 
+      log('event: $event');
       if (success) {
         completer.complete(request);
       } else {
@@ -45,6 +45,7 @@ class HttpClient implements BaseHttpClient {
 
     await completer.future;
 
+    log('response: ${request.response as ByteBuffer}');
     return (request.response as ByteBuffer).asUint8List();
   }
 }
